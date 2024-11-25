85a56cdc83940a500d05ce8e70e4b902fa9b378d
sergio <sergioruizmontilla@gmail.com>
2024-11-25 18:19:55 +0100

    updated standAloneTest file

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -23,7 +23,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//new comments are added
 		String productName = "ZARA COAT 3";
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver = new EdgeDriver();
